ec585d438996115caddff79588fb7d5a9edbf3ba
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-03-04 21:32:59 +0000

    Pin sticks into cushion

---
+++ Assets/scripts/Pin.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+[RequireComponent (typeof (Rigidbody2D))]
+public class Pin : MonoBehaviour {
+
+	[SerializeField]
+    private float speed;
+
+    private Rigidbody2D rb;
+    private bool isPinned;
+
+    private void Awake ()
+    {
+        rb = GetComponent<Rigidbody2D> ();
+    }
+
+    private void Update ()
+    {
+        if (!isPinned)
+        {
+            rb.MovePosition (rb.position + Vector2.up * speed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.tag == "rotator")
+        {
+            isPinned = true;
+            transform.parent = other.transform;
+        }
+    }
+
+}
---
+++ Assets/scripts/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+	[SerializeField]
+    private float speed = 100f;
+
+    private void Update ()
+    {
+        transform.Rotate (0f, 0f, speed * Time.deltaTime);
+    }
+
+}
